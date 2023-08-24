7c6430f6b0b7ff4318c47de62c244496ec16a022
Drinkmilk <141216114+lndvng@users.noreply.github.com>
2023-08-23 23:19:42 -0700

    completed acceptance criteria for js and added comments

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,66 +1,56 @@
-// Assignment Code
-/* ## Acceptance Criteria
-// I will click generate button
-// then I will be asked for pw criteria
-// i will ask user for pw length, casing,
-// numbers, or special characters
-// after i collect criteria, at least one 
-// casing, numbers or special char must be true
-// if all are false, im going to tell them they
-// must choose at least one 
-// when criteria is satisfied, i will generate pw
-// once pw is generated, i will display it on 
-// screen for the user
 
-// define variables with arrays
-// var with empty array
-// var w name password = empty array from ln 16
-
-// for loop
-// Math.floor(Math.random() * array.length) 
-
-// if, else if
-// 2 array methods array.push array.join
-*/
-
-/* Creating a function for the writePassword function to call */
+/* Created a function for the writePassword function to call */
 function generatePassword() {
-/* Defining variables for pw criteria */
+/* Defined variables for pw criteria */
     const lowerCase = ["a","b","c","d","e","f","g","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     const number = ["0","1","2","3","4","5","6","7","8","9"];
     const specialChar = ["!","@","#","$","%","^","&","*","(",")"];
-/* Creating empty array */
-    const emptyArray = [];
-    const password = [];
-/* Establishing min and max password length */
-    //let pwLength = (); // find verbage for user input other than confirm
+/* Created empty array */
+    let emptyArray = [""];
+    let password = [""];
+/* Established min and max password length */
+    let pwLength = prompt("How long would you like your password to be?");
+    if (isNaN(pwLength) || pwLength < 8 || pwLength > 128) {
+        alert('Password length must be provided as a number. Must be between 8 to 128 characters.');
+    }
+/* yes/no questions for users to select password criteria. if they click ok, returns value of true. if they click cancel, returns value of false. */
+    else {
+    var lowerCaseSelect = confirm("Do you want to use lowercase letters in your password?");
+    var upperCaseSelect = confirm("Do you want to use uppercase letters in your password?");
+    var numberSelect = confirm("Do you want to use numbers in your password?");
+    var specialCharSelect = confirm("Do you want to use special characters in your password?");
+    }
 
-    let lowerCaseSelect = confirm("Do you want to use lowercase letters in your password?");
     if (lowerCaseSelect) {
-        emptyArray.concat(lowerCase);
+        emptyArray = emptyArray.concat(lowerCase);
     }
-    let upperCaseSelect = confirm("Do you want to use uppercase letters in your password?");
+
     if (upperCaseSelect) {
-        emptyArray.concat(upperCase);
+        emptyArray = emptyArray.concat(upperCase);
     }
-    let numberSelect = confirm("Do you want to use nunbers in your password?");
+
     if (numberSelect) {
-        emptyArray.concat(number);
+        emptyArray = emptyArray.concat(number);
     }
-    let specialCharSelect = confirm("Do you want to use lowercase letters in your password?");
+
     if (specialCharSelect) {
-        emptyArray.concat(specialChar);
+        emptyArray = emptyArray.concat(specialChar);
     }
 
-function createPassword(){
-    let password = "";
-    Math.random() * (max - min) + max + min;
-    //Math.floor(Math.random) goes here
+/* Alert for when no password criteria is selected; happens when lowercase, uppercase, number, selectchar are all false. */
+    if (!lowerCaseSelect && !upperCaseSelect && !numberSelect && !specialCharSelect) {
+        alert ('Must select at least one: lowercase, uppercase, number, special character to be used in password. Please try again.');
+    }
+
+    emptyArray = emptyArray.toString();
+/* generates password: random character generated one by one (keeping the previous character(s)) until the number of characters satifies the same amount of password length input by user */
+    for (i = 0; i < pwLength; i++) {
+        password += emptyArray[Math.floor(Math.random() * emptyArray.length)];
+    }
+    return password;
 }
 
-
-}
 
 var generateBtn = document.querySelector("#generate");
 
